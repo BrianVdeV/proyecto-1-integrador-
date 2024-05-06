a3f5a8410a7b25d75e6ee03e01261952bd887531
PeterSonSistemas <pitersonalcoser@gmail.com>
2024-05-05 19:47:29 -0500

    primer commit

---
+++ src/main/java/peter/integrador/supertpet/SupertPet.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package peter.integrador.supertpet;
+import Interfaces.*;
+/**
+ *
+ * @author JESSY
+ */
+public class SupertPet {
+
+    public static void main(String[] args) {
+        Login log = new Login();
+        log.setVisible(true);
+    }
+}
